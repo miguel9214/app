7c01f5a4a4f92ce9895e02c10375d0af84ab7545
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2022-10-19 22:08:01 -0500

    creacion de app react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const persona={
+    nombre: 'tom',
+    apellido:'Jerry',
+    edad: 25
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function mostrarNombre(persona) {
+    console.log(`Su nombre es ${persona.nombre}`)
+    
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+mostrarNombre(persona);
